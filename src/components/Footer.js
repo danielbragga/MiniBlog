20b11fd68bcbdc1494a0d2276ce775eaf5c3e82f
Daniel Braga <danielbraga2121@gmail.com>
2023-01-16 23:33:03 -0300

    commit-two

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <h3>Escreva sobre o que você tem interesse</h3>
-      <p> Mini Blog &copy; 2023</p>
+      <p> Mini Blog &copy; 2023 AGORA VAI!</p>
     </footer>
   );
 };
